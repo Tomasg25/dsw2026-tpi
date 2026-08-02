b67ba9db123399895888100b433d0e624e792e5b
Lautaro <Lautaro.Herrera@alu.frt.utn.edu.ar>
2026-08-02 19:40:10 -0300

    Correccion en GET/Appointments

--- Dsw2026Tpi.Application/Dtos/AppointmentModel.cs
+++ Dsw2026Tpi.Application/Dtos/AppointmentModel.cs
@@ -10,6 +10,9 @@ namespace Dsw2026Tpi.Application.Dtos
         public record PatientDto(long Dni);
         public record Response(Guid Id, string Status);
         public record PatientAppointmentDto(Guid Id, string Doctor, string Speciality, DateOnly Date, string StartTime, string EndTime, string Reason, string Status);
-        public record SearchResultDto(Guid Id, string Specialty, string Doctor, DateOnly Date, string AvailableTime, string Status);
+        public record SearchResultDto(Guid AppointmentsId, string AppointmentsStatus, SearchPatientDto Patient, SearchDoctorDto Doctor);
+        public record SearchPatientDto(long Dni, string FullName);
+        public record SearchDoctorDto(Guid DoctorId, string Name, SearchSpecialtyDto Specialty);
+        public record SearchSpecialtyDto(Guid SpecialtyId, string Name);
     }
 }
